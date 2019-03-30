755fb1aabf887b1fd7be620668cbfeb5ba1fba9c
andre-tlima <andre-tlima@hotmail.com>
2019-03-30 13:34:05 +0000

    adicionando testes para requisicoes http

--- lib/api_version_constraint.rb
+++ lib/api_version_constraint.rb
@@ -1,10 +1,11 @@
 class ApiVersionConstraint
-   def initialize(options)
-      @version = options[:version]
-       @default = options[:default]
-   end
+    def initialize(options)
+        @version = options[:version]
+        @default = options[:default]
+    end
     
     def matches?(req)
-        default || req.headers['Accept'].include?("aplication/vnd.projetofase8.v#{version}")
-    end    
+        @default || req.headers['Accept'].include?("application/vnd.projetofase8.v#{@version}") 
+    end
 end
+
